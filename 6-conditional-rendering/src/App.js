0e8b5672c3ca772933d4d0a8fd91e6d8245fa5cc
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-03 20:46:53 -0500

    init

---
+++ 6-conditional-rendering/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RenderingApp from "./components/RenderingApp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <RenderingApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ 6-conditional-rendering/src/components/RenderingApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RenderingApp() {
+  return <div></div>;
+}
+
+export default RenderingApp;
