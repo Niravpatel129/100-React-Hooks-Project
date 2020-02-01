f01d8eee9ccc9722d8b4586dee7d814949044797
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-31 19:03:12 -0500

    app10

---
+++ 10-render-api-data/src/components/TeamList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+import "./TeamList.css";
+
+function TeamList() {
+  return (
+    <div className="TeamList">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default TeamList;
