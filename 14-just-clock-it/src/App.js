32b748e97a2e3d3f6b100035dc499fde388c7b3e
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-21 01:33:51 -0500

    stage 3

--- 14-just-clock-it/src/App.js
+++ 14-just-clock-it/src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import firebase from "./firebase";
-
-firebase
-  .firestore()
-  .collection("times")
-  .add({
-    user: "asd",
-    password: "asd"
-  });
+import clockList from "./components/clockList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Just Clock it</h1>
+      <clockList />
     </div>
   );
 }
---
+++ 14-just-clock-it/src/components/clockList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import firebase from "../firebase";
+
+function clockList() {
+  return <div></div>;
+}
+
+export default clockList;
