8dd8e0f140ddf83521f649fcd79b478625972425
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-22 07:08:12 -0500

    charge up d-e-c

--- 14-just-clock-it/src/App.js
+++ 14-just-clock-it/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import clockList from "./components/clockList";
+import ClockList from "./components/ClockList";
 
 function App() {
   return (
     <div className="App">
-      <clockList />
+      <ClockList />
     </div>
   );
 }
--- 14-just-clock-it/src/components/clockList.js
+++ 14-just-clock-it/src/components/clockList.js
@@ -1,8 +1,25 @@
 import React from "react";
 import firebase from "../firebase";
 
-function clockList() {
-  return <div></div>;
+function ClockList() {
+  return (
+    <div>
+      <h3>Times List</h3>
+      <div className="Select">
+        Sort By:
+        <select>
+          <option>Fastest</option>
+          <option>Slowest</option>
+          <option disabled>---</option>
+          <option>A-Z</option>
+          <option>Z-A</option>
+        </select>
+      </div>
+      <div className="TimeEntry">
+        <code>Hello Darkness - 44seconds</code>
+      </div>
+    </div>
+  );
 }
 
-export default clockList;
+export default ClockList;
