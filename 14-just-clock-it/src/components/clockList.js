060c4b35cbc9f82f7462e5b47fb9753d8da03c0e
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-22 23:56:29 -0500

    finished product

--- 14-just-clock-it/src/components/clockList.js
+++ 14-just-clock-it/src/components/clockList.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from "react";
 import firebase from "../firebase";
 import Addtime from "./Addtime";
 
-const SORT_OPTIONS = {};
+const SORT_OPTIONS = {
+  TIME_ASC: { column: "time", direction: "asc" },
+  TIME_DESC: { column: "time", direction: "desc" },
+  TITLE_ASC: { column: "title", direction: "asc" },
+  TITLE_DESC: { column: "title", direction: "desc" }
+};
 
 function ClockList() {
   const [times, setTimes] = useState([]);
+  const [sortBy, setSortBy] = useState("TIME_ASC");
 
   useEffect(() => {
     const unsub = firebase
       .firestore()
       .collection("times")
+      .orderBy(SORT_OPTIONS[sortBy].column, SORT_OPTIONS[sortBy].direction)
       .onSnapshot(ss => {
         let newTimes = ss.docs.map(data => {
           return { id: data.id, ...data.data() };
@@ -19,9 +26,13 @@ function ClockList() {
       });
 
     return () => {
-      unsub.unsubscribe();
+      if (unsub?.unsubscribe) unsub.unsubscribe();
     };
-  }, []);
+  }, [sortBy]);
+
+  const handleOptionChange = e => {
+    setSortBy(e.target.value);
+  };
 
   return (
     <div>
@@ -29,12 +40,12 @@ function ClockList() {
       <h3>Times List</h3>
       <div className="Select">
         Sort By:
-        <select>
-          <option>Fastest</option>
-          <option>Slowest</option>
+        <select onChange={handleOptionChange}>
+          <option value="TIME_ASC">Slowest</option>
+          <option value="TIME_DESC">Fastest</option>
           <option disabled>---</option>
-          <option>A-Z</option>
-          <option>Z-A</option>
+          <option value="TITLE_ASC">A-Z</option>
+          <option value="TITLE_DESC">Z-A</option>
         </select>
       </div>
       <div className="TimeEntry">
