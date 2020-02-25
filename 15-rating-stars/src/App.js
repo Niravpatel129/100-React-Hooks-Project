4c8371275436f3fcb2a1df938115f2804dac891a
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-25 12:24:55 -0500

    app init

--- 15-rating-stars/src/App.js
+++ 15-rating-stars/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Add a Rating: <div className="stars"></div>
+    </div>
+  );
 }
 
 export default App;
